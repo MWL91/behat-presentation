523aa2ac966f18d4bdb18f3d453bd82a9d9bfd8b
Marcin Lenkowski <m.lenkowski@asper.net.pl>
2020-10-29 17:15:17 +0100

    stage 4

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,6 +1,7 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
